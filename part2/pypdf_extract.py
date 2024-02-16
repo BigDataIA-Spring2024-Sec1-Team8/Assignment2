63aaf4dc0c2efcbae8bc5a2949ef3d07c8784046
Shivani Guglani <guglani.s@northeastern.edu>
2024-02-16 14:23:57 -0500

    Update part2 py pdf

--- part2/pypdf_extract.py
+++ part2/pypdf_extract.py
@@ -5,18 +5,21 @@ import boto3
 from dotenv import load_dotenv
 load_dotenv()
 
+#Extracting the text from the provided PDFs
 def extract_text_from_pdf(pdf_path):
     text = ''
     
     with open(pdf_path, 'rb') as file:
         pdf_reader = PyPDF2.PdfReader(file)
         
+        #Extracting text from all pages
         for page_number in range(len(pdf_reader.pages)):
             page = pdf_reader.pages[page_number]
             text += page.extract_text()
             
     return text
 
+#Writing the extracted text to a txt file
 def save_text_to_txtfile(pdf_path, text):
     # Extract the PDF name without the extension
     pdf_name = os.path.splitext(os.path.basename(pdf_path))[0]
@@ -24,12 +27,15 @@ def save_text_to_txtfile(pdf_path, text):
     with open(txt_filename, "w", encoding="utf-8") as txt_file:
         txt_file.write(text)
 
+#Paths which lead to the PDFs from which text is to be extracted
 pdf_paths = ['./resources/Level1_combined.pdf', './resources/Level2_combined.pdf', './resources/Level3_combined.pdf']
 
+#Initiating a loop for getting every path 
 for pdf_path in pdf_paths:
     extracted_text = extract_text_from_pdf(pdf_path)
     save_text_to_txtfile(pdf_path, extracted_text)
 
+#Uploading the txt file to S3 from local
 def upload_folder_to_s3(local_folder_path, s3_bucket_name, s3_folder_path, aws_access_key_id, aws_secret_access_key):
     # Create an S3 client
     s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
